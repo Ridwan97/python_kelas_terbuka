e9dab40a3ebd080e81e2ac304fef6c2c65566750
Muhammad Ridwan <ridwan.sastrawijaya@gmail.com>
2021-10-24 20:47:56 +0700

    first commit

---
+++ 00 - Template/03 - Alur Kerja Python/main.py
@@ -0,0 +1,20 @@
+import time
+start_time = time.time()
+
+print("Hello")
+print("World")
+print("Hello World")
+
+print("Hallo Chantiek")
+#ini adalah comment
+a= 10 #ini juga adalah comment
+""" ada apa dengan 
+ucup dan otong si ganteng 
+dalam comment multiline"""
+print(a)
+print(time.time() - start_time, "detik")
+
+# kita bisa mengcompile python ke
+# yang namanya bytecode
+# cara mencompile, buka terminal dan tuliskan
+# python -m py_compile Main.py
---
+++ 00 - Template/09 - latihan perhitungan sederhana/main.py
@@ -0,0 +1,21 @@
+# latihan konversi satuan temperatur
+
+# program konversi celcius ke satuan lain 
+
+print("\nPROGRAM KONVERSI TEMPERATUR\n")
+
+# Celcius
+celcius = float(input('Masukkan suhu dalam celcius : '))
+print("suhu adalah",celcius, "Celcius")
+
+# Reamur
+reamur = (4/5) * celcius
+print("suhu dalam reamur adalah",reamur, "Reamur")
+
+# Fahrenheit
+fahrenheit = ((9/5) * celcius) + 32
+print("suhu dalam fahrenheit adalah",fahrenheit, "Fahrenheit")
+
+# Kelvin
+kelvin = celcius + 273
+print("suhu dalam kelvin adalah",kelvin, "Celcius")
