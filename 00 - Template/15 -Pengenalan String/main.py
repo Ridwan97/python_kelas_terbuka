2107766c4ff0f258b61de3e3b97c9fb590171682
Muhammad Ridwan <ridwan.sastrawijaya@gmail.com>
2021-10-25 10:46:06 +0700

    latihan 14.5

---
+++ 00 - Template/15 -Pengenalan String/main.py
@@ -0,0 +1,22 @@
+data = "ini adalah string"
+print(data)
+print(type(data))
+
+# 1. cara membuat string
+
+'''
+    1. dengan menggunakan single quote '...' 
+    2. dengan menggunakan double qupte "..."
+'''
+
+data = 'menggunakan single quote'
+print(data)
+
+data = "menggaunakan double quote"
+print(data)
+
+print('"Halo, apa Kabar?"')
+print("'Halo, apa kabar?'")
+print("ini adalah hari juma`t")
+
+# mengg
